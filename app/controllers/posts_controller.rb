57d38707a03dde2727c34ed5de263283484e6ad6
Maha-Magdy <mahamagdy3@gmail.com>
2021-05-30 09:31:14 +0200

    Add user devise views, and connect it to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
   before_action
 
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "Post was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post)
   end
 end
